e454dfc8aae097e91d4855693c96d1c6e2f911ab
Mümin Mete Acet <110824381+meteAcet@users.noreply.github.com>
2025-02-18 14:17:00 +0300

    adding task for vs code
    added tasks for build and run that project

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
-#include <gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 // Vertex Shader
 const char* vertexShaderSource = R"(
