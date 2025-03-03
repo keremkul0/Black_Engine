7a982b05b07fc8fed4f8b341c09fbbc80007d0b6
Kerem Kul <keremkul0@outlook.com>
2025-03-03 10:39:01 +0300

    gitignore fix

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Kerem/CLionProjects/Black_Engine/tests
-# Build directory: C:/Users/Kerem/CLionProjects/Black_Engine/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Kerem/CLionProjects/Black_Engine/build/tests/unit_tests[1]_include.cmake")
